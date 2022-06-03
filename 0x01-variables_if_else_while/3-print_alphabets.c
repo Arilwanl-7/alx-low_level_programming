3aaba000925bdddaad40ff929925596fb304da7f
Arilwanl-7 <engrrilwan7@gmail.com>
2022-06-03 12:35:35 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print alphabet in lc,uc
+ * Return: 0
+ */
+int main(void)
+{
+  char a = 'a';
+
+  while (a <= 'z')
+    {
+      putchar(a);
+      a++;
+    }
+
+  a = 'A';
+
+  while (a <= 'Z')
+    {
+      putchar(a);
+      a++;
+    }
+
+  putchar('\n');
+  return (0);
+}
