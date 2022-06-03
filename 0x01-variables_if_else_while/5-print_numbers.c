1e3f9ba966725948f6f214aca69b66ce7e2c85ea
Arilwanl-7 <engrrilwan7@gmail.com>
2022-06-03 13:48:13 -0700

    task 5 and 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: prints all single digit numbers of base 10 starting 0
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+printf("%i", a);
+printf("\n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: prints all single digit numbers of base 10 starting 0
+ * Return: 0
+ */
+int main(void)
+{
+int i = 12;
+
+while (i < 17)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
